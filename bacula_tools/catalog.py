6d8170bbf983255eb640253ba7bcc31537dcd3ee
Brian Gallew <geek@gallew.org>
2013-11-20 07:17:21 -0800

    docstring updates

--- bacula_tools/catalog.py
+++ bacula_tools/catalog.py
@@ -47,6 +47,8 @@ class Catalog(DbDict):
     # {{{ __str__(): 
 
     def __str__(self):
+        '''Convert a Catalog into its string representation.  The result can be
+        inserted directly into the configuration of the director.'''
         self.output = ['Catalog {\n  Name = "%(name)s"' % self,'}']
         
         for key in self.SETUP_KEYS:
@@ -57,6 +59,10 @@ class Catalog(DbDict):
     # {{{ search(key=None):
 
     def search(self, key=None):
+        '''Override the standard self.search() function to look up a catalog by
+        Director ID.  This is primarily used when generating the
+        configuration for a director, as you'll have a Director ID and need
+        to look up the associated catalog.'''
         DbDict.search(self, key)
         if self[ID]: return self
         if not self[DIRECTOR_ID]: return self # can't look myself up if I don't have any attributes
@@ -67,16 +73,18 @@ class Catalog(DbDict):
         return self
 
     # }}}
-    # {{{ _cli_special_setup(): add in password support
+    # {{{ _cli_special_setup(): add in Director ID support
 
     def _cli_special_setup(self):
-        self.parser.add_option('--director', help='The name or ID of the director that uses this catalog')
+        self.parser.add_option('--director', help='The name or ID of the Director that uses this catalog')
         return
 
     # }}}
-    # {{{ _cli_special_do_parse(args): handle password parsing
+    # {{{ _cli_special_do_parse(args): handle Director ID parsing
 
     def _cli_special_do_parse(self, args):
+        '''It should be noted that a Catalog can be associated with one, and only
+        one, Director.  This function handles changing the Director from the CLI.'''
         if args.director == None: return # Nothing to do!
         d = bacula_tools.Director()
         d.search(args.director)
@@ -91,6 +99,7 @@ class Catalog(DbDict):
     # {{{ _cli_special_print(): print out passwords
 
     def _cli_special_print(self):
+        '''De-reference the Director and print its name.'''
         d = bacula_tools.Director().search(self[DIRECTOR_ID])
         fmt = '%' + str(self._maxlen) +'s: %s'
         print(fmt % (DIRECTOR, d[NAME]))
@@ -98,6 +107,7 @@ class Catalog(DbDict):
 
     # }}}
 
+# Implement the CLI for managing Catalogs
 def main():
     s = Catalog()
     s.cli()

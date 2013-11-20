a95433bf23743b0ef5d6642a142ac7362468d810
Brian Gallew <geek@gallew.org>
2013-11-20 07:16:49 -0800

    minor docstring update

--- bacula_tools/bacula_director.py
+++ bacula_tools/bacula_director.py
@@ -2,7 +2,7 @@ from . import *
 # client for interacting with a the Bacula director
 
 class BDirector(BSock):
-    '''Client for communicating directly with a file daemon.
+    '''Client for communicating directly with a director.
     '''
 # {{{ __init__(director_object, timeout=5):
     def __init__(self, director_object, debug=False, timeout=5):

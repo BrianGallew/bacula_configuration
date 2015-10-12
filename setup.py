1a8b381210c3fdf3184649f1467e1cdaab7a4830
Brian Gallew <bgallew@llnw.com>
2015-10-12 16:13:54 -0700

    Fix the hash output to be valid

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import glob
 from setuptools import setup, find_packages
 
 NAME = 'bacula_configuration'
-VERSION = '0.96'
+VERSION = '0.97'
 WEBSITE = 'http://gallew.org/bacula_configuration'
 LICENSE = 'GPLv3 or later'
 DESCRIPTION = 'Bacula configuration management tool'
